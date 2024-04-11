ff66ad5838d0fc83a76130404021ae5080c5aab7
Eshhha <nguyenhoanganhgoahead@gmail.com>
2024-04-11 11:47:28 +0700

    Delete src/main/java/com/library/sub directory

--- src/main/java/com/library/sub/ExampleApp.java
+++
@@ -1,9 +0,0 @@
-// ExampleApp.java - ExampleApp interface
-package com.library.sub;
-import java.net.URL;
-
-public interface ExampleApp {
-    default URL getViewLocation() {
-        return getClass().getResource("primary.fxml");
-    }
-}
